cb462e2172cfc8163b9b1ffdf942038a74129873
STANSLOUS <musayalous@gmail.com>
2026-04-01 21:29:02 +0200

    project core completed users are now getting the admins approval before the can enter the system wow we did it

--- app/route/admin.py
+++ app/route/admin.py
@@ -1,5 +1,7 @@
 from flask import Blueprint
 from flask import render_template
+from flask import redirect
+from flask import url_for
 
 from app.model.user import User
 from app.model.user import database
@@ -10,3 +12,15 @@ admin_bp = Blueprint('admin_bp', __name__)
 def admin():
     all_users = User.query.all()
     return render_template("admin_approval_side.html", users=all_users)
+
+@admin_bp.route('/admin/active_deactivate/<int:user_id>', methods=['GET', 'POST'])
+def active_deactivate(user_id):
+    user = User.query.get(user_id)
+    if not user.is_active:
+        user.is_active = True
+        database.session.commit()
+        return redirect(url_for('admin_bp.admin'))
+    else:
+        user.is_active = False
+        database.session.commit()
+        return redirect(url_for('admin_bp.admin'))
