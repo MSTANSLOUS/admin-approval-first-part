7492020bb4cd62a689ebc97dbb0773f23e24cddd
STANSLOUS <musayalous@gmail.com>
2026-04-01 17:24:50 +0200

    updated the route (user.py) it is now able to receive incoming form data and just print them for now

--- app/route/user.py
+++ app/route/user.py
@@ -1,8 +1,19 @@
 from flask import Blueprint
 from flask import render_template
+from flask import request
 
 user_bp = Blueprint('user_bp', __name__)
 
 @user_bp.route('/')
 def home_page():
+    return render_template('index.html')
+
+@user_bp.route('/login_page', methods=['GET', 'POST'])
+def login_page():
+    name  = request.form.get('user_name')
+    email = request.form.get('user_email')
+
+    print(name)
+    print(email)
+
     return render_template('index.html')
